efd1ce876180bdd832e8ac71bc5240ea22262691
SAMIDANNAT <59145694+SAMIDANNAT@users.noreply.github.com>
2020-01-27 19:30:14 +0200

    Add files via upload

---
+++ problema3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class problema3 ;
+{
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	Double c=sc.nextDouble();
+	Double d=c+(c*2/100);
+        System.out.println("Dat fiind ca se pierd 2 procente din greutatea lor,cantitatea de fructe proaspete pe care trebuie sa le procuram este "+d);
+	}
+}
